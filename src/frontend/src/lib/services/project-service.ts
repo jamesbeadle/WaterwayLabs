8116bf0ec8c44577e7f2c804b1f7bd96ae354e5c
James Beadle <jamesbeadle1989@gmail.com>
2024-11-03 15:22:28 +0000

    Add project service and store

---
+++ src/frontend/src/lib/services/project-service.ts
@@ -0,0 +1,21 @@
+import { idlFactory } from "../../../../declarations/backend";
+import type { ProjectDTO } from "../../../../declarations/backend/backend.did";
+import { ActorFactory } from "../../utils/ActorFactory";
+import { isError } from "../utils/helpers";
+
+export class ProjectService {
+  private actor: any;
+
+  constructor() {
+    this.actor = ActorFactory.createActor(
+      idlFactory,
+      process.env.BACKEND_CANISTER_ID,
+    );
+  }
+
+  async getProjects(): Promise<ProjectDTO[]> {
+    const result = await this.actor.getProjects();
+    if (isError(result)) throw new Error("Failed to fetch projects.");
+    return result.ok;
+  }
+}
--- src/frontend/src/lib/stores/project-store.ts
+++ src/frontend/src/lib/stores/project-store.ts
@@ -1,4 +1,14 @@
 import { writable } from "svelte/store";
-import type { Project } from "../../types";
+import type { ProjectDTO } from "../../../../declarations/backend/backend.did";
+import { ProjectService } from "$lib/services/project-service";
 
-export const selectedProjectStore = writable<Project | null>(null);
+function createProjectStore() {
+  const { subscribe, set } = writable<ProjectDTO[]>([]);
+
+  return {
+    subscribe,
+    setProjects: (projects: ProjectDTO[]) => set(projects)
+  };
+}
+
+export const projectStore = createProjectStore();
