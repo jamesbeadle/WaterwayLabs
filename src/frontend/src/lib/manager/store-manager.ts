f538b18124f55791b2ea503327f9e147a4811139
James Beadle <jamesbeadle1989@gmail.com>
2024-11-03 14:38:18 +0000

    Add store manager

---
+++ src/frontend/src/lib/manager/store-manager.ts
@@ -0,0 +1,64 @@
+import { DataHashService } from "$lib/services/data-hash-service";
+
+import { isError, replacer } from "$lib/utils/helpers";
+
+class StoreManager {
+  private dataHashService: DataHashService;
+  private projectService: ProjectService;
+  
+  private categories: string[] = [
+    "projects"
+  ];
+
+  constructor() {
+    this.dataHashService = new DataHashService();
+    this.projectService = new ProjectService();
+  }
+
+  async syncStores(): Promise<void> {
+    const newHashes = await this.dataHashService.getDataHashes();
+
+    let error = isError(newHashes);
+    if (error) {
+      console.error("Error fetching data hashes.");
+      return;
+    }
+
+    for (const category of this.categories) {
+      const categoryHash = newHashes.find((hash) => hash.category === category);
+
+      if (categoryHash?.hash !== localStorage.getItem(`${category}_hash`)) {
+        await this.syncCategory(category);
+        localStorage.setItem(`${category}_hash`, categoryHash?.hash || "");
+      } else {
+        this.loadFromCache(category);
+      }
+    }
+  }
+
+  private async syncCategory(category: string): Promise<void> {
+    switch (category) {
+      case "countries":
+        const updatedProjects = await this.projectService.getProjects();
+        projectStore.setProjects(updatedProjects);
+        localStorage.setItem(
+          "projects",
+          JSON.stringify(updatedProjects, replacer),
+        );
+        break;
+    }
+  }
+
+  private loadFromCache(category: string): void {
+    const cachedData = localStorage.getItem(category);
+
+    switch (category) {
+      case "projects":
+        const cachedProjects = JSON.parse(cachedData || "[]");
+        countryStore.setCountries(cachedProjects);
+        break;
+    }
+  }
+}
+
+export const storeManager = new StoreManager();
