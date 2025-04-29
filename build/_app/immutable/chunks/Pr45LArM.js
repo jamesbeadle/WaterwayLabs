48cab4e0109f036629a8587a9198e7382f90ffcf
jamesbeadle <jamesbeadle1989@gmail.com>
2025-04-29 07:55:42 +0100

    Update admin views

--- build/_app/immutable/chunks/Pr45LArM.js
+++
@@ -1,1 +0,0 @@
-import{s as e,p as r}from"./D-P6p6x8.js";const t={get error(){return r.error},get status(){return r.status},get url(){return r.url}};e.updated.check;const a=t;export{a as p};
--- src/frontend/src/routes/admin/+page.svelte
+++ src/frontend/src/routes/admin/+page.svelte
@@ -1,10 +1,22 @@
 <script lang="ts">
+    import { onMount } from "svelte";
     import ProjectDetail from "$lib/components/project/project-detail.svelte";
     import ProjectLogs from "$lib/components/project/project-logs.svelte";
-    import { projectStore } from "$lib/stores/project-store";
+    import { ProjectService } from "$lib/services/project-service";
+    import type { Project } from "../../../../declarations/backend/backend.did";
+
+    const projectService = new ProjectService();
 
     let activeTab: string = $state("details");
     let selectedProjectId = $state(0);
+
+    let projects: Project[] = $state([]);
+
+    onMount(async () => {
+        const projectsResult = await projectService.getProjects();
+        if(!projectsResult) {return }
+        projects = projectsResult.projects;
+    });
 
     function setActiveTab(tabName: string) {
         activeTab = tabName;
@@ -17,12 +29,11 @@
     
 </script>
 
-<p>Select Project:</p>   
 <select class="p-2 brand-dropdown my-4 min-w-[100px]"
     bind:value={selectedProjectId}
 >
     <option value={0}>Select Project</option>
-    {#each $projectStore as project}
+    {#each projects as project}
     <option value={project.id}>{project.name}</option>
     {/each}
 </select>
