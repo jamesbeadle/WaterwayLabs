8ad2357f1fbba4fae663ae457f37f25245b45091
jamesbeadle <jamesbeadle1989@gmail.com>
2025-04-30 14:12:47 +0100

    Add copy principal

--- src/frontend/src/routes/profile/+page.svelte
+++ src/frontend/src/routes/profile/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+    import CopyPrincipal from "$lib/components/shared/copy-principal.svelte";
   import { authStore } from "$lib/stores/auth-store";
   import { onMount } from "svelte";
 
@@ -8,4 +9,11 @@
     
   });
 </script>
+  
+<div class="flex flex-col space-y-2 p-4">
   <h1>Profile</h1>
+
+  <CopyPrincipal backgroundColour='#2D5F73' borderColor='white' isProfile=true />
+
+</div>
+
