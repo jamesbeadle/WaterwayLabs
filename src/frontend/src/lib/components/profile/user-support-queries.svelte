91ff045e21332bded25aff87f5a2d60552301901
jamesbeadle <jamesbeadle1989@gmail.com>
2025-04-30 16:39:03 +0100

    Add user support query views

---
+++ src/frontend/src/lib/components/profile/user-support-queries.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+    import { onMount } from "svelte";
+    import type { Project, ProjectId, SupportQueries } from "../../../../../declarations/backend/backend.did";
+    import { projectStore } from "$lib/stores/project-store";
+    import LocalSpinner from "../shared/local-spinner.svelte";
+    import { supportStore } from "$lib/stores/support-store";
+
+    let project: Project | undefined = $state(undefined);
+    let supportQueries: SupportQueries | undefined = $state(undefined);
+
+    onMount(async () => {
+      await getSupportQueries();
+    });
+
+    async function getSupportQueries(){
+      supportQueries = await supportStore.getUserSupportQueries({
+        page: 1n
+      });
+    }
+</script>
+
+
+{#if project}
+  <div class="flex flex-col space-y-2 xs:space-y-4 mx-4 mt-6 xs:mt-8 sm:mt-6 lg:mt-24">
+
+    {#if supportQueries}
+      {#each supportQueries.supportQueries as supportQuery}
+        <div class="flex items-center justify-between w-full">
+          <p>{supportQuery.id}</p>
+          <p>{supportQuery.status}</p>
+          <p>{supportQuery.contact}</p>
+          <p>{supportQuery.assignedTo}</p>
+          <p>{supportQuery.name}</p>
+          <p>{supportQuery.submittedOn}</p>
+          <p>{supportQuery.submittedBy}</p>
+          <p>{supportQuery.message}</p>
+        </div>
+      {/each}
+    {/if}
+  </div>
+{:else}
+  <LocalSpinner />
+{/if}
+
+
