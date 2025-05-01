79ebf23a7089ec58566583e6f9d4e59dfe53d78a
jamesbeadle <jamesbeadle1989@gmail.com>
2025-05-01 08:45:45 +0100

    Remove unused files

--- src/frontend/src/lib/components/project/create-project-modal.svelte
+++ src/frontend/src/lib/components/project/create-project-modal.svelte
@@ -1,9 +1,7 @@
 <script lang="ts">
     import LocalSpinner from "$lib/components/shared/local-spinner.svelte";
-    import type { Snippet } from "svelte";
     import Modal from "../shared/modal.svelte";
     
-
     interface Props {
         onClose: () => void;
         title: string;
@@ -18,32 +16,31 @@
     }
   </script>
   
-  <Modal {onClose} {title} {visible} >
-    
-  <div class="flex items-center justify-start w-full">
-    {#if isLoading}
-      <LocalSpinner />
-    {:else}
-        <div class="flex-col w-full mb-2 space-y-4">\
+<Modal {onClose} {title} {visible} >
+    <div class="flex items-center justify-start w-full">
+        {#if isLoading}
+        <LocalSpinner />
+        {:else}
+            <div class="flex-col w-full mb-2 space-y-4">\
 
-            {#if !isSubmitDisabled}
-                <div class="flex items-center">
-                    <p class="text-orange-400">
-                    Are you sure you want to create this project?
-                    </p>
-                </div>
-                <div class="flex items-center">
-                    <button
-                    class={`${isSubmitDisabled ? "brand-button-disabled" : "brand-button"} px-4 py-2 w-full`}
-                    onclick={createProject}
-                    disabled={isSubmitDisabled}
-                    >
-                    Create Project
-                    </button>
-                </div>
-            {/if}
+                {#if !isSubmitDisabled}
+                    <div class="flex items-center">
+                        <p class="text-orange-400">
+                        Are you sure you want to create this project?
+                        </p>
+                    </div>
+                    <div class="flex items-center">
+                        <button
+                        class={`${isSubmitDisabled ? "brand-button-disabled" : "brand-button"} px-4 py-2 w-full`}
+                        onclick={createProject}
+                        disabled={isSubmitDisabled}
+                        >
+                        Create Project
+                        </button>
+                    </div>
+                {/if}
 
-        </div>
-    {/if}
-  </div>
+            </div>
+        {/if}
+    </div>
 </Modal>
